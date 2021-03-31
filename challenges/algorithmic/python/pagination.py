baac2f022bae62732714184ca6d2e66a4e45d32e
Sujesh Menon <smenon@tcd.ie>
2021-03-31 04:13:46 +0100

    google pagination impl

--- challenges/algorithmic/python/pagination.py
+++ challenges/algorithmic/python/pagination.py
@@ -1,7 +1,4 @@
 def get_pages(c, t, o=10):
-    k = o // 2 + 1
+    k = [o // 2 + 1, o // 2 + 2][o % 2 != 0]
     n = t - o // 2 + 1
-    return [x + 1 + [0, c - k][c > k] - [0, (c - n)][c > n] for x in range(o)]
-
-
-print(get_pages(15, 10, 5))
+    return [x + 1 + [0, c - k][c > k] - [0, c - n][c > n] for x in range(o)]
